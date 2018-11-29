f8b34c76bed21b0226845d4607abe0207d173613
Philipe Barra <philipebarra@gmail.com>
2018-11-29 16:00:44 -0300

    EventListner and mouse, now styling div options

--- VanillaSelect.js
+++ VanillaSelect.js
@@ -6,7 +6,7 @@ class VanillaSelect {
 
         this._originalSelect.style.display = 'none';
 
-        //inserting the table inside a div
+        //inserting the select inside a div
         this._divParent = document.createElement('div');
         this._divParent.classList.add(__tc.DIV_CLASS);
         this._originalSelect.parentNode.insertBefore(this._divParent, this._originalSelect);
@@ -32,14 +32,20 @@ class VanillaSelect {
         this._selectedElement = null;
 
 
-        this._input.onclick = () => this._click();
+        // this._input.onclick = (e) => this._click(e);
         this._input.onkeydown = () => this._analiseEvent();
+
+        window.addEventListener('click', (e) => this._click(e));
     }
 
-    _click() {
-        if (!this.isVisible()) {//show options
-            this.show(this._allOptions);
-            this._selectedIndex = null;
+    _click(e) {
+        if(this._input.contains(e.target)) {//input clicked
+            this.show(this._input);
+        } else if(!this._allOptions.contains(e.target)) {//click outside options
+            this.hide(this._allOptions);
+        } else if(this._allOptions.contains(e.target)) {
+            this.hide(this._allOptions);
+            this._selectElement(e.target);
         }
     }
 
@@ -84,17 +90,15 @@ class VanillaSelect {
     }
 
     hide() {
-        this._allOptions.classList.remove(__tc.VISIBLE);
-        this._allOptions.classList.add(__tc.HIDDE);
+        this._allOptions.classList.add(__tc.HIDE);
     }
 
     show() {
-        this._allOptions.classList.remove(__tc.HIDDE);
-        this._allOptions.classList.add(__tc.VISIBLE);
+        this._allOptions.classList.remove(__tc.HIDE);
     }
 
     isVisible() {
-        return this._allOptions.classList.contains(__tc.VISIBLE);
+        return !this._allOptions.classList.contains(__tc.HIDE);
     }
 
     _getElement(index) {
@@ -126,8 +130,7 @@ class VanillaSelect {
 //Table constants
 let __tc = {
     DIV_CLASS: '___select',//div that wraps the table
-    ALL_OPTIONS: '___all_options ___select_hidde',
-    VISIBLE: '___select_visible',
-    HIDDE: '___select_hidde',
+    ALL_OPTIONS: '___all_options ___select_hide',
+    HIDE: '___select_hide',
 };
 Object.freeze(__tc);
