447407149a403f6f7f4e53fd499f6bad61585f2e
Philipe <philipebarra@gmail.com>
2018-12-04 07:56:05 -0300

    better style like bootstrap 4

--- VanillaSelect.js
+++ VanillaSelect.js
@@ -1,46 +1,61 @@
 class VanillaSelect {
 
+    /*
+     * <div class="col-4">
+     *  <input id="p" class='form-control ___select-icon-input'>
+     *  <label for="p"><img src="caret-down-solid.svg" class='___select-caret-down-icon'></label>
+     * </div>
+     */
     constructor(id) {
         this._originalSelect = document.getElementById(id);
         if (this._originalSelect === null) return;
 
-        this._originalSelect.style.display = 'none';
+        this._originalSelect.style.display = 'visible';
 
-        //inserting the select inside a div
         this._divParent = document.createElement('div');
-        this._divParent.classList.add(__tc.DIV_CLASS);
-        this._originalSelect.parentNode.insertBefore(this._divParent, this._originalSelect);
+
+        this._divParent.classList = 'col-sm-4 nopadding';
 
         this._input = document.createElement('input');
-        this._input.type = 'text';
-        this._input.autocomplete = 'off';
-        this._input.classList = this._originalSelect.classList;
+        this._input.classList = __tc.INPUT;
+        this._input.id = '___select' + Math.random().toString(36).substring(7);
         this._divParent.appendChild(this._input);
 
+        this._label = document.createElement('label');
+        this._label.setAttribute('for', this._input.id);
+        this._divParent.appendChild(this._label);
+
+        this._img = document.createElement('img');
+        this._img.src = 'caret-down-solid.svg';
+        this._img.classList = __tc.ICON;
+        this._label.appendChild(this._img);
+
+        this._originalSelect.parentNode.insertBefore(this._divParent, this._originalSelect);
+
         this._allOptions = document.createElement('div');
-        this._allOptions.classList = `${__tc.ALL_OPTIONS}`;
+        this._allOptions.classList = __tc.OPTIONS;
         this._divParent.appendChild(this._allOptions);
 
         let options = this._originalSelect.querySelectorAll('option');
         let total = options.length;
         for (let i = 0; i < total; i++) {
+            // console.log(options[i].classList.add('list-group-item'));
+            // options[i].classList.add(tc_.OPTIONITEM);
             this._allOptions.appendChild(options[i]);
         }
-        // this._divParent.appendChild(this._originalSelect);
 
-        this._selectedIndex = null;
-        this._selectedElement = null;
-
-
-        // this._input.onclick = (e) => this._click(e);
         this._input.onkeydown = () => this._analiseEvent();
-
         window.addEventListener('click', (e) => this._click(e));
+
     }
 
     _click(e) {
         if(this._input.contains(e.target)) {//input clicked
-            this.show(this._input);
+            if(this.isVisible()) {
+                this.hide();
+            } else {
+                this.show();
+            }
         } else if(!this._allOptions.contains(e.target)) {//click outside options
             this.hide(this._allOptions);
         } else if(this._allOptions.contains(e.target)) {
@@ -53,13 +68,13 @@ class VanillaSelect {
         let e = window.event;
         let element = null;
 
-        if (e.keyCode === 27) {//esc
+        if (e.key === 27) {//esc
             this.hide(this._allOptions);
             this._selectedIndex = null;
             return;
         }
 
-        if(e.keyCode === 38) {//up
+        if(e.key === 38) {//up
             if (this._selectedIndex === null) {
                 this._selectedIndex = 0;
             } else {
@@ -68,7 +83,7 @@ class VanillaSelect {
             element = this._getElement(this._selectedIndex);
         }
 
-        if (e.keyCode === 40) {//down
+        if (e.key === 40) {//down
             if (!this.isVisible()) {//show options
                 this.show(this._allOptions);
                 this._selectedIndex = null;
@@ -83,7 +98,7 @@ class VanillaSelect {
         }
 
         
-        // if(e.keyCode === 13 && this._selectedIndex) {//enter
+        // if(e.key === 13 && this._selectedIndex) {//enter
         //     this._selectElement 
         // }
         console.log(element);
@@ -129,8 +144,12 @@ class VanillaSelect {
 
 //Table constants
 let __tc = {
-    DIV_CLASS: '___select',//div that wraps the table
-    ALL_OPTIONS: '___all_options ___select_hide',
-    HIDE: '___select_hide',
+    ROOT: '___select-root',
+    OPTIONS: '___select-all-options ___select-hide',
+    OPTIONITEM: '',
+    ICON: '___select-caret-down-icon',
+    INPUT: 'form-control ___select-icon-input',
+    HIDE: '___select-hide'
+    // WRAPPER_IMG: '___wrapper ___img'
 };
 Object.freeze(__tc);
